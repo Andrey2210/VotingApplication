25dd091698201cbe34894c85da07ee5c914c6a54
Andrey2210 <bv.andrey2210@mail.ru>
2017-06-09 02:21:35 +0300

    .gitignore tune

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO voting_app.t_user (f_email, f_password) VALUES ('andrey@gmail.com', '$2a$11$uwn.hHQwgLtJ8qUB6AuLaOzHdDavpcibxyXg7AO8AvekGbC0tILiy');
-
-INSERT INTO voting_app.t_voting (f_question, f_state, f_user_id)  VALUES ('Как дела?', 'ACTIVE', 1);
-
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Хорошо', 0, 1);
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Нормально', 0, 1);
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Плохо', 0, 1);
