a985a3cd28cad6fb3f6dd7590d40a1deeac8040c
Andrey2210 <bv.andrey2210@mail.ru>
2017-06-13 20:14:58 +0300

    Added all functionality and bug fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO voting_app.t_user (f_email, f_password) VALUES ('andrey@gmail.com', '$2a$11$uwn.hHQwgLtJ8qUB6AuLaOzHdDavpcibxyXg7AO8AvekGbC0tILiy');
 
-INSERT INTO voting_app.t_voting (f_question, f_state, f_user_id)  VALUES ('Как дела?', 'ACTIVE', 1);
+INSERT INTO voting_app.t_voting (f_question, f_state, f_user_id)  VALUES ('How are you?', 'ACTIVE', 1);
 
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Хорошо', 50, 1);
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Нормально', 18, 1);
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Плохо', 5, 1);
+INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Fine', 50, 1);
+INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Good', 18, 1);
+INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Not bad', 5, 1);
 
-INSERT INTO voting_app.t_voting (f_question, f_state, f_user_id)  VALUES ('Кого больше?', 'ACTIVE', 1);
+INSERT INTO voting_app.t_voting (f_question, f_state, f_user_id)  VALUES ('Who is more?', 'ACTIVE', 1);
 
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Я Парень', 1300, 2);
-INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('Я Девушка', 800, 2);
+INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('I am man' 1300, 2);
+INSERT INTO voting_app.t_answer (f_text, f_voters_number, f_voting_id) VALUES ('I am woman', 800, 2);
